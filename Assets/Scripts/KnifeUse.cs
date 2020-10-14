7a2940841c9ffdf7191e8bfce6edeec5ee0dfdf6
charlietran01 <charlietran01@gmail.com>
2020-10-14 12:10:31 -0500

    Added assets for kitchen tools/items

---
+++ Assets/Scripts/KnifeUse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeUse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bread;
+    public GameObject cuttingBoard;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
