d2b17ebdd7a613d0aedf6d21a58a016938f64a29
charlietran01 <charlietran01@gmail.com>
2020-10-16 11:14:38 -0500

    Ctreated Stage two knife use

--- Assets/Scripts/KnifeUse.cs
+++ Assets/Scripts/KnifeUse.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 public class KnifeUse : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject bread;
-    public GameObject cuttingBoard;
+    public GameObject other;
+    public GameObject blade;
+    bool handColliding;
+    GameObject hand;
     void Start()
     {
-        
-    }
 
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<Rigidbody>()) 
+        {
+            hand = other.gameObject;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        hand = null;
+    }
+    public void GrabObject()
+    {
+        other = hand;
+        other.transform.SetParent (this.transform);
+        other.GetComponent<Rigidbody>().isKinematic = true;
+    }
+    private void ReleaseObject()
+    {
+        other.GetComponent<Rigidbody>().isKinematic = false;
+        other.transform.SetParent (null);
+        other = null;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") > 0.2f && hand != null) 
+        {
+            GrabObject();
+        }
+        if(Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") < 0.2f && other != null) 
+        {
+            ReleaseObject();
+        }
     }
 }
